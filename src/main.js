e498f8ba37f9557a47b21d6c3a91f7aefc379ed4
6-ON <aminhatim6@gmail.com>
2023-02-24 22:33:08 +0100

    add pania

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import './axios.config'
 import './assets/main.css'
 
 const app = createApp(App)
+const pania = createPinia()
 
-app.use(createPinia())
+pania.use(({ store }) => {
+    store.router = markRaw(router)
+})
+
+app.use(pania)
 app.use(router)
 
 app.mount('#app')
