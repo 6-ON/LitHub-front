a781b8db0a07608a92d2ce13e8a34e9cb2c0f795
6-ON <aminhatim6@gmail.com>
2023-02-27 23:05:39 +0100

    group page

---
+++ src/pages/Groups.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { getGroups } from '@/api/groups';
+import { ref, onMounted } from 'vue';
+import { getGroups } from '@/api/groups';
+const groups = ref();
+
+onMounted(() => {
+    groups.value = getGroups();
+})
+</script>
+<template>
+    <div v-for="group in groups">
+        {{ group.title }}
+    </div>
+</template>
