f430d92968236e6a463ac3fa2efa1b765f96388f
6-ON <aminhatim6@gmail.com>
2023-02-27 23:06:04 +0100

    login vue

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -11,41 +11,31 @@ const form = ref({
 
 </script>
 <template>
-    <div
-        class="relative flex min-h-screen text-gray-800 antialiased flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-        <div class="relative py-3 sm:w-96 mx-auto text-center">
-            <span class="text-2xl font-light ">Login to your account</span>
-            <div class="mt-4 bg-white shadow-md rounded-lg text-left">
-                <div class="h-2 bg-purple-400 rounded-t-md"></div>
-                <div class="px-8 py-6 ">
-                    <label class="block font-semibold"> Username or Email </label>
-                    <input type="text" placeholder="Email"
-                        class="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md">
-                    <label class="block mt-3 font-semibold"> Username or Email </label>
-                <input type="password" placeholder="Password"
-                    class="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md">
-                <div class="flex justify-center items-baseline">
-                    <button type="submit"
-                        class="mt-4 bg-purple-500 text-white py-2 px-6 rounded-md hover:bg-purple-600 ">Login</button>
-                        
-                    </div>
-                </div>
-
-            </div>
-        </div>
+    <div class="w-full h-screen flex flex-col justify-center items-center font-mono">
+        <form @submit.prevent="">
+            <RouterLink to="/">
+                <img src="@/assets/image/logo.png" class="h-24 mb-3">
+            </RouterLink>
+            <input class="form-input" placeholder="email">
+            <input class="form-input" type="password" placeholder="password">
+            <span class="text-gray-800 mt-3">
+                Unregistred ?
+                <RouterLink class="hover:underline text-slate-600" to="/register"> Register</RouterLink>
+            </span>
+            <button type="submit" class="form-submit">Login</button>
+        </form>
     </div>
-        <!-- <h1 class="text-3xl">Login</h1>
-            <form @submit.prevent="authStore.login(form)">
-                <input type="email" v-model="form.email" class="form-input">
-                <input type="password" v-model="form.password" class="form-input">
-                <button type="submit" class="p-4 bg-slate-400 rounded-xl">Login</button>
-            </form> -->
 </template>
 <style scoped>
 .form-input {
-    @apply bg-slate-400 rounded-lg w-48
+    @apply bg-slate-50 shadow rounded-lg w-64 h-8 p-5 placeholder-gray-600 focus:outline-slate-400 outline-none
+}
+
+.form-submit {
+    @apply bg-black py-3 text-white rounded-2xl px-10 shadow-xl hover:bg-white hover:text-black active:shadow-inner mt-16
 }
 
 form {
     @apply flex flex-col items-center mx-auto gap-3 mt-6
-}</style>
+}
+</style>
