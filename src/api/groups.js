21aa97c1f38481ee0b535a7ab895dcf1230a4869
6-ON <aminhatim6@gmail.com>
2023-03-03 11:27:12 +0100

    add groups methods

--- src/api/groups.js
+++ src/api/groups.js
@@ -1,12 +1,44 @@
-async function getGroups(params) {
+import axios from "axios";
+async function getGroups() {
     try {
         const response = await axios.get('/api/groups');
         return response.data;
     } catch (error) {
-        console.log(error.response)
+        console.log(error)
         return [];
 
     }
 }
+async function getGroup(id) {
+    try {
+        const response = await axios.get(`/api/group/${id}`);
+        return response.data;
+    } catch (error) {
+        console.log(error)
+        return null;
 
-export { getGroups };
+    }
+}
+async function sendGroupMessage(data) {
+    try {
+        const response = await axios.post('/api/message/',data);
+        return response.data;
+    } catch (error) {
+        console.log(error)
+        return false;
+
+    }
+}
+async function joinGroup(id){
+    try {
+        const response = await axios.post(`/api/group/${id}/join`);
+        return response.status === 201;
+    } catch (error) {
+        console.log(error)
+        return false;
+
+    }
+}
+
+
+export { getGroups, getGroup,sendGroupMessage,joinGroup };
