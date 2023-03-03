dd9b104d9c495531253777517b20a35281be36e0
6-ON <aminhatim6@gmail.com>
2023-03-03 11:27:46 +0100

    add some routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import BooksVue from '@/pages/Books.vue'
+import GroupsVue from '@/pages/Groups.vue'
 import HomeVue from '@/pages/Home.vue'
 import LoginVue from '@/pages/Login.vue'
 import RegisterVue from '@/pages/Register.vue'
+import BookVue from '@/pages/Book.vue'
+import groupVue from '@/pages/GroupOverview.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { getGroup } from '@/api/groups'
+import ProfileVue from '@/pages/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,32 +15,85 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeVue
+      component: HomeVue,
+      meta: {
+        title: 'Home'
+      }
+
     },
     {
       path: '/login',
       name: 'login',
-      component: LoginVue
+      component: LoginVue,
+      meta: {
+        title: 'Login'
+      }
 
     },
     {
       path: '/register',
       name: 'register',
-      component: RegisterVue
+      component: RegisterVue,
+      meta: {
+        title: 'Register'
+      }
+
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileVue,
+      meta: {
+        title: 'Profile'
+      }
+
+    },
+    {
+      path: '/books',
+      name: 'books',
+      component: BooksVue,
+      meta: {
+        title: 'Books'
+      }
 
     }
     ,
     {
-      path: '/books',
-      name: 'books',
-      component: BooksVue
+      path: '/book:/:id',
+      name: 'book',
+      component: BookVue,
+      meta: {
+        title: 'Book'
+      }
 
     }
     ,
     {
       path: '/groups',
       name: 'groups',
-      component: null
+      component: GroupsVue,
+      meta: {
+        title: 'Groups'
+      }
+
+    }
+    ,
+    {
+      path: '/group/:id',
+      name: 'group',
+      component: groupVue,
+      meta: {
+        title: 'Group'
+      },
+      beforeEnter: async (to, from) => {
+        const group = await getGroup(to.params.id)
+        if (group) {
+          to.params.group = group
+        } else {
+          alert('join first')
+          return false
+        }
+      }
 
     }
   ]
@@ -44,6 +102,9 @@ const router = createRouter({
 
 
 // })
+router.afterEach((to, from, next) => {
+  document.title = to.meta.title || 'LitHub'
+})
 
 
 export default router
