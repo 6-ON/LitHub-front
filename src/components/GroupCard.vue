dca8335b20598a35a01670c15b072a707d948abb
6-ON <aminhatim6@gmail.com>
2023-03-03 11:21:02 +0100

    create group card

---
+++ src/components/GroupCard.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { joinGroup } from '@/api/groups'
+import { useRouter } from 'vue-router';
+const router = useRouter()
+const props = defineProps({
+    group: {
+        type: Object,
+        required: true
+    }
+})
+
+async function handleJoin() {
+    const joined = await joinGroup(props.group.id)
+    if (joined) {
+        router.push(`/group/${props.group.id}`)
+    }
+}
+</script>
+
+<template>
+    <div class="card max-w-sm self-auto mx-auto bg-white rounded-md overflow-hidden shadow-md hover:shadow-lg">
+        <div class="relative">
+            <router-link :to="'/group/' + group.id">
+                <img class="w-full h-64 object-cover" :src="group.image" alt="Group Image">
+            </router-link>
+            <div class="absolute top-0 right-0 mt-2 mr-2">
+                <span class="px-2 py-1 text-white bg-blue-500 rounded-full">{{ group.members_count }}</span>
+            </div>
+        </div>
+        <div class="px-4 py-3">
+            <router-link :to="'/group/' + group.id">
+                <h3 class="text-lg font-semibold text-gray-800 hover:text-blue-500">{{ group.title }}</h3>
+            </router-link>
+            <p class="text-gray-600">{{ group.description }}</p>
+        </div>
+        <button @click="handleJoin" :class="{ 'btn-disabled': group.is_joined }" class="m-3  btn btn-accent ">{{
+            group.is_joined ? 'joined' :
+            'join' }}</button>
+    </div>
+</template>
+<style scoped></style>
