6508360dcf9ba37630fcace775eb6cc27b5cf4d8
6-ON <aminhatim6@gmail.com>
2023-03-03 11:27:36 +0100

    create date helpers

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -2,7 +2,10 @@ const rtf = new Intl.RelativeTimeFormat('en', { style: 'narrow' });
 const CONV = 3600 * 24 * 1000
 const getElapsedTime = (date_str) => {
     const date = new Date(date_str)
-    return rtf.format(Math.round(((date - new Date()) / (CONV))),'day') 
+    const diff = Math.round(((date - new Date()) / (CONV)))
+    if (diff == 0) 
+        return 'today';
+    return rtf.format(diff,'day') 
 }
 
 export { getElapsedTime };
