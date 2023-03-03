b8c986d3ce6925d6bfe32c7252793be46ff39114
6-ON <aminhatim6@gmail.com>
2023-03-03 23:30:28 +0100

    add alert sucees

---
+++ src/components/common/AlertSuccess.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="alert alert-success text-white">
+        <div>
+            <span>{{ message }}</span>
+        </div>
+    </div>
+</template>
+<script setup>
+defineProps({
+    message: {
+        required: true,
+        type: String
+    }
+})
+</script>
