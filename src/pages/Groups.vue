50ba71e360cc65b22b244c0d4e495abe1cb31041
6-ON <aminhatim6@gmail.com>
2023-03-03 11:25:15 +0100

    finish groups page

--- src/pages/Groups.vue
+++ src/pages/Groups.vue
@@ -1,15 +1,16 @@
 <script setup>
 import { getGroups } from '@/api/groups';
+import GroupCard from '@/components/GroupCard.vue';
 import { ref, onMounted } from 'vue';
-import { getGroups } from '@/api/groups';
 const groups = ref();
-
-onMounted(() => {
-    groups.value = getGroups();
+onMounted(async () => {
+    groups.value = await getGroups();
 })
 </script>
 <template>
-    <div v-for="group in groups">
-        {{ group.title }}
+    <h1 class="text-3xl font-bold text-center p-8 text-sky-600">Groups</h1>
+
+    <div class="grid grid-cols-1 p-3 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <GroupCard v-for="group in groups" :group="group"></GroupCard>
     </div>
 </template>
