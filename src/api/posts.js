f194112c78e22c071a2ffa3a5b826913cb3b0095
6-ON <aminhatim6@gmail.com>
2023-03-03 11:27:21 +0100

    posts methods

--- src/api/posts.js
+++ src/api/posts.js
@@ -5,10 +5,34 @@ const getPosts = async function () {
         const response = await axios.get('/api/posts');
         return response.data;
     } catch (error) {
-        console.log(error.response)
+        console.log(error.data)
+        return [];
+
+    }
+
+}
+const getPost = async function (id) {
+    try {
+        const response = await axios.get(`/api/post/${id}`);
+        return response.data;
+    } catch (error) {
+        console.log(error.data)
         return [];
 
     }
 }
+const commentPost = async function (comment) {
+    console.log(comment);
+    try {
+        const response = await axios.post('/api/comment',{
+            post_id :comment.post,
+            content :comment.content
+        })
+        return response.data;
+    } catch (error) {
+        console.log(error.data)
+        return false
+    }
+}
 
-export { getPosts };
+export { getPosts, getPost,commentPost };
