46698c9f39a4c8220a94392c0c28935cd69a0bb9
6-ON <aminhatim6@gmail.com>
2023-03-05 04:21:19 +0100

    create category api file

---
+++ src/api/category.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+async function createCategory(data) {
+    try {
+        const response = await axios.post('/api/category',{
+            label: data.label
+        })
+        return response.data
+    } catch (error) {
+        console.log(error.response);
+        return false
+    }
+}
+
+export { createCategory }
