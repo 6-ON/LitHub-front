99ac0e5829df5cac099ddcb5688ccd903917d862
6-ON <aminhatim6@gmail.com>
2023-03-05 14:19:16 +0100

    hide post & categories btns from users and guests

--- src/pages/Books.vue
+++ src/pages/Books.vue
@@ -6,9 +6,10 @@ import BookCard from '@/components/BookCard.vue';
 import CreatePostModal from '@/components/CreatePostModal.vue'
 import CreateCategoryModal from '@/components/CreateCategoryModal.vue'
 import { useLibStore } from '@/stores/lib';
-
+import { useAuthStore } from '@/stores/auth';
 const posts = ref()
 const libStore = useLibStore()
+const authStore = useAuthStore()
 onMounted(async () => {
     libStore.getCategories()
     posts.value = await getPosts();
@@ -21,7 +22,7 @@ onMounted(async () => {
 <template>
     <div class="w-full flex  overflow-x-scroll scrollbar-none p-3 gap-2 ">
 
-        <label for="create-category-modal" class="p-2 border rounded-full hover:bg-gray-300 cursor-pointer">
+        <label v-if="authStore.user?.isAdmin" for="create-category-modal" class="p-2 border rounded-full hover:bg-gray-300 cursor-pointer">
             <svg class="w-6" aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 4.5v15m7.5-7.5h-15" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -34,9 +35,9 @@ onMounted(async () => {
     <div class="box-border grid mx-4 gap-3  grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <BookCard v-for="post in posts" :post="post"></BookCard>
     </div>
-    <CreatePostModal></CreatePostModal>
-    <CreateCategoryModal></CreateCategoryModal>
-    <div class="fixed bottom-6 right-4">
+    <CreatePostModal v-if="authStore.user?.isAdmin" ></CreatePostModal>
+    <CreateCategoryModal v-if="authStore.user?.isAdmin" ></CreateCategoryModal>
+    <div v-if="authStore.user?.isAdmin" class="fixed bottom-6 right-4">
         <label for="create-post-modal" class="btn btn-circle btn-lg">
             <svg class="w-8" aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg">
