3f99f4f3aff8af792a8fdeafa1823feddc4bfcc8
6-ON <aminhatim6@gmail.com>
2023-03-05 04:26:02 +0100

    add info icon

--- src/pages/GroupOverview.vue
+++ src/pages/GroupOverview.vue
@@ -31,7 +31,12 @@ async function handleSend(data) {
     </div>
     <h1 class="mt-3 text-3xl text-center text-blue-500 font-bold">
         {{ group?.title }}
-        <label for="group-info-modal" class="badge ">info</label>
+        <label for="group-info-modal" class="badge ">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 stroke-current">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            </svg>
+        </label>
     </h1>
 
     <!-- group info modal -->
