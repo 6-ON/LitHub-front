85218d048989800f9ab07bb92f845ff0c6e805cc
6-ON <aminhatim6@gmail.com>
2023-03-28 01:22:51 +0000

    feat: create Favourite.vue

---
+++ src/components/common/Favourite.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="text-amber-400 w-8 h-8" @click="$emit('changed')">
+        <svg aria-hidden="true" :fill="isFavourite? 'currentColor':'none'" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+    </button>
+</template>
+
+<script  setup>
+const emit = defineEmits(['changed']);
+
+const props = defineProps({
+    isFavourite: {
+        type: Boolean,
+        required: true
+    }
+});
+
+</script>
